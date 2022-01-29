89825adc81d5b187150bffa462dad3dcab5f45e1
dylancoffey99 <dylancoffey99@gmail.com>
2022-01-29 02:56:14 +0000

    Created load_graphs() with a canvas to hold a figure, which will contain a subplot. Fixed the various padding of widgets to incorporate this. Changed styling of header buttons to change colour when active.

--- iotumble/views/home_view.py
+++ iotumble/views/home_view.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 
 from PIL import Image, ImageTk
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from iotumble.views.abstract_view import AbstractView
 
@@ -12,6 +14,8 @@ class HomeView(AbstractView, tk.Tk):
         self.home_controller = controller
         self.frames = [tk.Frame(), tk.Frame(), tk.Frame(), tk.Frame(), tk.Frame()]
         self.header_logo = ImageTk.PhotoImage(Image.open("logo.png"))
+        self.tree_views = [ttk.Treeview(), ttk.Treeview()]
+        self.details_columns = ["Timestamp", "X-Acceleration", "Y-Acceleration", "Z-Acceleration"]
 
     def load_root(self):
         self.title("IoTumble")
@@ -28,8 +32,8 @@ class HomeView(AbstractView, tk.Tk):
                     "anchor": "center", "background": self.primary_bg, "borderwidth": 0,
                     "font": (self.font, 16, "bold"), "foreground": self.primary_fg
                 }, "map": {
-                    "background": [("pressed", self.primary_fg)],
-                    "foreground": [("pressed", self.secondary_bg)]
+                    "background": [("active", self.primary_fg)],
+                    "foreground": [("active", self.secondary_bg)]
                 }},
             "TLabel": {
                 "configure": {
@@ -49,7 +53,7 @@ class HomeView(AbstractView, tk.Tk):
                 "configure": {
                     "background": self.primary_bg, "borderwidth": 0,
                     "fieldbackground": self.secondary_bg, "font": (self.font, 12),
-                    "foreground": self.primary_fg, "rowheight": 50
+                    "foreground": self.primary_fg, "rowheight": 44
                 }, "map": {
                     "background": [("selected", self.primary_fg)],
                     "foreground": [("selected", self.secondary_bg)]
@@ -58,7 +62,7 @@ class HomeView(AbstractView, tk.Tk):
                 "configure": {
                     "background": self.primary_bg, "borderwidth": 0,
                     "fieldbackground": self.secondary_bg, "font": (self.font, 10),
-                    "foreground": self.primary_fg, "rowheight": 25
+                    "foreground": self.primary_fg, "rowheight": 24
                 }},
             "details.Treeview.Heading": {
                 "configure": {
@@ -79,11 +83,11 @@ class HomeView(AbstractView, tk.Tk):
 
     def load_frames(self):
         self.frames[0] = tk.Frame(self, background=self.primary_bg)
-        self.frames[0].pack(expand=True, fill="both", side="top")
+        self.frames[0].pack(expand=True, fill="both", side="top", ipady=20)
         self.frames[1] = tk.Frame(self, background=self.secondary_bg)
-        self.frames[1].pack(expand=True, fill="both", side="left", ipady=150)
+        self.frames[1].pack(expand=True, fill="both", side="left")
         self.frames[2] = tk.Frame(self, background=self.secondary_bg)
-        self.frames[2].pack(expand=True, fill="both", side="top", ipadx=650)
+        self.frames[2].pack(expand=True, fill="both", side="top", ipadx=324)
         self.frames[3] = tk.Frame(self, background=self.primary_bg)
         self.frames[3].pack(expand=True, fill="both", side="left")
         self.frames[4] = tk.Frame(self, background=self.primary_bg)
@@ -91,38 +95,43 @@ class HomeView(AbstractView, tk.Tk):
 
     def load_header(self):
         header_logo_label = ttk.Label(self.frames[0], image=self.header_logo)
-        header_logo_label.pack(fill="both", side="left", padx=30)
+        header_logo_label.pack(fill="both", side="left", padx=44)
         header_exit_button = ttk.Button(self.frames[0], text="Exit", takefocus=False,
                                         command=self.close)
         header_exit_button.pack(fill="both", side="left", ipadx=40)
 
     def load_incidents(self):
-        incidents_tree_view = ttk.Treeview(self.frames[1], show="tree", selectmode="browse",
-                                           style="incidents.Treeview")
-        incidents_tree_view.pack(expand=True, fill="both", side="left")
+        self.tree_views[0] = ttk.Treeview(self.frames[1], show="tree", selectmode="browse",
+                                          style="incidents.Treeview")
+        self.tree_views[0].pack(expand=True, fill="both", side="left")
         incidents_scrollbar = ttk.Scrollbar(self.frames[1], orient="vertical",
-                                            command=incidents_tree_view.yview)
+                                            command=self.tree_views[0].yview)
         incidents_scrollbar.pack(fill="both", side="right")
-        incidents_tree_view.configure(yscrollcommand=incidents_scrollbar.set)
-        incidents_tree_view.tag_configure("0", background=self.primary_bg)
-        incidents_tree_view.tag_configure("1", background=self.secondary_bg)
+        self.tree_views[0].configure(yscrollcommand=incidents_scrollbar.set)
+        self.tree_views[0].tag_configure("0", background=self.secondary_bg)
+        self.tree_views[0].tag_configure("1", background=self.primary_bg)
 
     def load_details(self):
         details_label = ttk.Label(self.frames[3], text="Incident Details")
         details_label.pack()
-        details_columns = ["Timestamp", "X-Acceleration", "Y-Acceleration", "Z-Acceleration"]
-        details_tree_view = ttk.Treeview(self.frames[3], columns=details_columns, show="headings",
-                                         selectmode="none", style="details.Treeview")
-        details_tree_view.pack(expand=True, fill="both", side="left")
+        self.tree_views[1] = ttk.Treeview(self.frames[3], columns=self.details_columns,
+                                          show="headings", selectmode="none",
+                                          style="details.Treeview")
+        self.tree_views[1].pack(expand=True, fill="both", side="left")
         details_scrollbar = ttk.Scrollbar(self.frames[3], orient="vertical",
-                                          command=details_tree_view.yview)
+                                          command=self.tree_views[1].yview)
         details_scrollbar.pack(fill="both", side="right")
-        details_tree_view.configure(yscrollcommand=details_scrollbar.set)
-        details_tree_view.tag_configure("0", background=self.primary_bg)
-        details_tree_view.tag_configure("1", background=self.secondary_bg)
-        for column in details_columns:
-            details_tree_view.column(column, anchor="center", width=1)
-            details_tree_view.heading(column, text=column)
+        self.tree_views[1].configure(yscrollcommand=details_scrollbar.set)
+        self.tree_views[1].tag_configure("0", background=self.secondary_bg)
+        self.tree_views[1].tag_configure("1", background=self.primary_bg)
+        for column in self.details_columns:
+            self.tree_views[1].column(column, anchor="center", width=1)
+            self.tree_views[1].heading(column, text=column)
+
+    def load_graphs(self):
+        fig = plt.figure(facecolor=self.secondary_bg)
+        canvas = FigureCanvasTkAgg(fig, master=self.frames[2]).get_tk_widget()
+        canvas.pack(fill="both")
 
     def start(self):
         self.load_root()
@@ -131,6 +140,7 @@ class HomeView(AbstractView, tk.Tk):
         self.load_header()
         self.load_incidents()
         self.load_details()
+        self.load_graphs()
         self.mainloop()
 
     def close(self):
