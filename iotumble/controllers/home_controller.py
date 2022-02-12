7e4a6477d61c49091823b6e159084d607a69618c
dylancoffey99 <dylancoffey99@gmail.com>
2022-02-12 06:12:24 +0000

    Implemented initial functionality to program. Created connect_session(), fill_incidents(), and renamed view() to switch() in HomeController. Created input dict to hold session entry values in HomeView. Created new methods get_input(), fill_input() and fill_incidents(). Changed highlight styling of buttons and incidents treeview.

--- iotumble/controllers/home_controller.py
+++ iotumble/controllers/home_controller.py
@@ -5,13 +5,26 @@ from iotumble.models.session import Session
 class HomeController(AbstractController):
     def __init__(self, config):
         self.home_view = self.load_view("Home")(self)
-        self.session = Session(config.get("config", "access_key_id"),
-                               config.get("config", "secret_access_key"),
-                               config.get("config", "region_name"))
+        self.home_view.fill_input(config.get("config", "access_key_id"),
+                                  config.get("config", "secret_access_key"),
+                                  config.get("config", "region_name"))
+        self.session = Session()
 
-    def view(self):
+    def connect_session(self):
+        if self.session.check_boto_session():
+            home_input = self.home_view.get_input()
+            self.session.connect(home_input[0], home_input[1], home_input[2])
+            self.session.create_table("iotumble_incidents")
+            self.fill_incidents()
+
+    def fill_incidents(self):
+        incident_count = self.session.request_incident_count()
+        self.home_view.fill_incidents(incident_count)
+
+    def switch(self, incident_id):
         self.home_view.hide()
-        incident_controller = self.load_controller("Incident")(self.home_view)
+        incident = self.session.request_incident(incident_id)
+        incident_controller = self.load_controller("Incident")(self.home_view, incident)
         incident_controller.main()
 
     def main(self):
--- iotumble/views/home_view.py
+++ iotumble/views/home_view.py
@@ -12,16 +12,16 @@ class HomeView(AbstractView, tk.Tk):
         self.withdraw()
         self.controller = controller
         self.icon = tk.Toplevel()
-        self.border = tk.Frame()
-        self.incidents_tree_view = ttk.Treeview()
         self.header_logo = ImageTk.PhotoImage(Image.open("logo.png"))
+        self.incidents_tree_view = ttk.Treeview()
+        self.input = {"access": tk.StringVar(), "secret": tk.StringVar(), "region": tk.StringVar()}
 
     def load_root(self):
         self.overrideredirect(True)
         self.set_geometry(self, 600, 600)
         self.configure(background=self.primary_bg)
-        self.border = tk.Frame(self, background=self.primary_bg, relief="solid", borderwidth=1)
-        self.border.pack(expand=True, fill="both")
+        self.frame = tk.Frame(self, background=self.primary_bg, relief="solid", borderwidth=1)
+        self.frame.pack(expand=True, fill="both")
 
     def load_icon(self):
         self.icon.attributes("-alpha", 0.0)
@@ -38,16 +38,16 @@ class HomeView(AbstractView, tk.Tk):
                     "anchor": "center", "background": self.primary_bg, "borderwidth": 0,
                     "font": (self.font, 12, "bold"), "foreground": self.primary_fg
                 }, "map": {
-                    "background": [("pressed", self.primary_fg)],
-                    "foreground": [("pressed", self.secondary_bg)]
+                    "background": [("pressed", self.highlight_bg)],
+                    "foreground": [("pressed", self.primary_fg)],
                 }},
             "header.TButton": {
                 "configure": {
                     "anchor": "center", "background": self.primary_bg, "borderwidth": 0,
                     "font": (self.font, 14, "bold"), "foreground": self.primary_fg
                 }, "map": {
-                    "background": [("active", self.primary_fg)],
-                    "foreground": [("active", self.secondary_bg)]
+                    "background": [("active", self.highlight_bg)],
+                    "foreground": [("active", self.primary_fg)]
                 }},
             "TCombobox": {
                 "configure": {
@@ -91,9 +91,6 @@ class HomeView(AbstractView, tk.Tk):
                     "background": self.primary_bg, "borderwidth": 0,
                     "fieldbackground": self.secondary_bg, "font": (self.font, 12),
                     "foreground": self.primary_fg, "rowheight": 46
-                }, "map": {
-                    "background": [("selected", "black")],
-                    "foreground": [("selected", self.primary_fg)]
                 }},
             "details.Treeview": {
                 "configure": {
@@ -124,17 +121,18 @@ class HomeView(AbstractView, tk.Tk):
         self.tk.eval("ttk::style configure ComboboxPopdownFrame -relief solid")
 
     def load_header(self):
-        header_logo_label = ttk.Label(self.border, style="primary.TLabel", image=self.header_logo)
+        header_logo_label = ttk.Label(self.frame, style="primary.TLabel", image=self.header_logo)
         header_logo_label.pack(expand=True, fill="both", side="top", ipady=24)
         header_logo_label.bind("<ButtonPress-1>", lambda e: self.pressed_window(self, True, e))
         header_logo_label.bind("<ButtonRelease-1>", lambda e: self.pressed_window(self, False, e))
         header_logo_label.bind("<B1-Motion>", lambda e: self.move_window(self, e))
 
     def load_incidents(self):
-        incidents_frame = tk.Frame(self.border, background=self.secondary_bg)
+        incidents_frame = tk.Frame(self.frame, background=self.secondary_bg)
         incidents_frame.pack(expand=True, fill="both", side="left")
-        incidents_label = ttk.Label(incidents_frame, style="tertiary.TLabel", text="Incidents")
-        incidents_label.pack(fill="both")
+        incidents_title_label = ttk.Label(incidents_frame, style="tertiary.TLabel",
+                                          text="Incidents")
+        incidents_title_label.pack(fill="both")
         self.incidents_tree_view = ttk.Treeview(incidents_frame, show="tree", selectmode="browse",
                                                 style="incidents.Treeview")
         self.incidents_tree_view.pack(expand=True, fill="both", side="left")
@@ -142,41 +140,75 @@ class HomeView(AbstractView, tk.Tk):
                                             command=self.incidents_tree_view.yview)
         incidents_scrollbar.pack(fill="both", side="right")
         self.incidents_tree_view.configure(yscrollcommand=incidents_scrollbar.set)
+        self.incidents_tree_view.tag_configure("highlight", background=self.highlight_bg,
+                                               foreground=self.primary_fg)
         self.incidents_tree_view.tag_configure("0", background=self.secondary_bg)
         self.incidents_tree_view.tag_configure("1", background=self.primary_bg)
+        self.incidents_tree_view.bind("<Motion>", lambda e: self.highlight_tree_view(True, e))
+        self.incidents_tree_view.bind("<Leave>", lambda e: self.highlight_tree_view(False, e))
+        self.incidents_tree_view.bind("<<TreeviewSelect>>", lambda e: self.switch())
 
     def load_session(self):
-        session_frame = tk.Frame(self.border, background=self.secondary_bg)
+        session_frame = tk.Frame(self.frame, background=self.secondary_bg)
         session_frame.pack(expand=True, fill="both", side="top", ipadx=47)
-        session_label = ttk.Label(session_frame, style="tertiary.TLabel", text="Session")
-        session_label.pack(fill="both")
+        session_title_label = ttk.Label(session_frame, style="tertiary.TLabel", text="Session")
+        session_title_label.pack(fill="both")
         session_access_label = ttk.Label(session_frame, style="secondary.TLabel",
                                          text="Access Key ID")
         session_access_label.pack(expand=True, pady=(18, 0))
-        session_access_entry = ttk.Entry(session_frame, font=(self.font, 12))
+        session_access_entry = ttk.Entry(session_frame, font=(self.font, 12), width=21,
+                                         textvariable=self.input["access"])
         session_access_entry.pack()
         session_secret_label = ttk.Label(session_frame, style="secondary.TLabel",
                                          text="Secret Access Key")
         session_secret_label.pack(expand=True)
-        session_secret_entry = ttk.Entry(session_frame, font=(self.font, 12))
+        session_secret_entry = ttk.Entry(session_frame, font=(self.font, 12), width=21,
+                                         textvariable=self.input["secret"])
         session_secret_entry.pack()
         session_region_label = ttk.Label(session_frame, style="secondary.TLabel",
                                          text="Region Name")
         session_region_label.pack(expand=True)
         session_region_combobox = ttk.Combobox(session_frame, state="readonly",
                                                font=(self.font, 12),
-                                               values=["us-east-1", "us-east-2",
-                                                       "us-west-1", "us-west-2"])
+                                               textvariable=self.input["region"])
+        session_region_combobox["values"] = ("us-east-1", "us-east-2", "us-west-1", "us-west-2")
         session_region_combobox.pack()
 
     def load_buttons(self):
-        buttons_frame = tk.Frame(self.border, background=self.secondary_bg)
+        buttons_frame = tk.Frame(self.frame, background=self.secondary_bg)
         buttons_frame.pack(expand=True, fill="both", side="bottom")
         connect_button = ttk.Button(buttons_frame, takefocus=False, style="login.TButton",
-                                    text="Connect", command=self.controller.view)
+                                    text="Connect", command=self.controller.connect_session)
         connect_button.pack(expand=True, fill="both", side="left", padx=(0, 5), pady=(46, 0))
         quit_button = ttk.Button(buttons_frame, takefocus=False, text="Quit", command=self.close)
         quit_button.pack(expand=True, fill="both", side="left", pady=(46, 0), ipadx=18)
+
+    def get_input(self):
+        return self.input["access"].get(), self.input["secret"].get(), self.input["region"].get()
+
+    def fill_input(self, access_key_id, secret_access_key, region_name):
+        self.input["access"].set(access_key_id)
+        self.input["secret"].set(secret_access_key)
+        self.input["region"].set(region_name)
+
+    def fill_incidents(self, incident_count):
+        for i in range(incident_count):
+            i += 1
+            self.incidents_tree_view.insert("", "end", tags=str(i % 2),
+                                            text="Incident " + str(i))
+
+    def highlight_tree_view(self, highlighted, event):
+        row = self.incidents_tree_view.identify_row(event.y)
+        self.tk.call(self.incidents_tree_view, "tag", "remove", "highlight")
+        if highlighted:
+            self.tk.call(self.incidents_tree_view, "tag", "add", "highlight", row)
+
+    def switch(self):
+        selected = self.incidents_tree_view.selection()[0]
+        if selected != "":
+            selected_text = self.incidents_tree_view.item(selected, "text")
+            incident_id = int(selected_text.replace("Incident ", ""))
+            self.controller.switch(incident_id)
 
     def hide(self):
         self.icon.unbind("<Map>")
