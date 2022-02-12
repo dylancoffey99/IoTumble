ae617c165fcbe5a3998454b42456e35856853301
dylancoffey99 <dylancoffey99@gmail.com>
2022-02-12 06:03:25 +0000

    Created get_incident_timestamp() in Incident. Shortened get method names in Timestamp, removed epoch variable, created get_date_time() and get_time().

--- iotumble/models/incident.py
+++ iotumble/models/incident.py
@@ -3,11 +3,15 @@ class Incident:
         self.incident_id = incident_id
         self.timestamps = timestamps
 
-    def __eq__(self, other):
-        return self.incident_id == other.incident_id
-
     def get_incident_id(self):
         return self.incident_id
 
     def get_timestamps(self):
         return self.timestamps
+
+    def get_incident_timestamp(self):
+        incident_svm = max(timestamp.get_svm() for timestamp in self.timestamps)
+        for timestamp in self.timestamps:
+            if timestamp.get_svm() == incident_svm:
+                return timestamp
+        return None
--- iotumble/models/timestamp.py
+++ iotumble/models/timestamp.py
@@ -1,23 +1,30 @@
+from datetime import datetime
+
+
 class Timestamp:
-    def __init__(self, timestamp_id, epoch, sensor_data):
+    def __init__(self, timestamp_id, timestamp_data):
         self.timestamp_id = timestamp_id
-        self.epoch = epoch
-        self.sensor_data = sensor_data
+        self.timestamp_data = timestamp_data
 
     def get_timestamp_id(self):
         return self.timestamp_id
 
-    def get_epoch(self):
-        return self.get_epoch
+    def get_x_acc(self):
+        return self.timestamp_data[0]
 
-    def get_x_acceleration(self):
-        return self.sensor_data[0]
+    def get_y_acc(self):
+        return self.timestamp_data[1]
 
-    def get_y_acceleration(self):
-        return self.sensor_data[1]
-
-    def get_z_acceleration(self):
-        return self.sensor_data[2]
+    def get_z_acc(self):
+        return self.timestamp_data[2]
 
     def get_svm(self):
-        return self.sensor_data[3]
+        return self.timestamp_data[3]
+
+    def get_date_time(self):
+        date_time = datetime.fromtimestamp(self.timestamp_data[4])
+        return date_time.strftime("%d %B %Y | %H:%M:%S.%f")[:-3]
+
+    def get_time(self):
+        time = datetime.fromtimestamp(self.timestamp_data[4])
+        return time.strftime("%H:%M:%S.%f")[:-3]
