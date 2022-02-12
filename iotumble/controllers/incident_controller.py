d145a7217bb33c9c6c7dd46358e5858c6f4f592f
dylancoffey99 <dylancoffey99@gmail.com>
2022-02-12 06:17:06 +0000

    Implemented functionality to details section of IncidentView. Created fill_details() in IncidentController, which now passes an incident object in constructor. Changed variables in constructor so IncidentView uses reference of icon/header_logo from HomeView. Created fill_details_labels() and fill_details_tree_view() and renamed some variables.

--- iotumble/controllers/incident_controller.py
+++ iotumble/controllers/incident_controller.py
@@ -2,9 +2,19 @@ from iotumble.controllers.abstract_controller import AbstractController
 
 
 class IncidentController(AbstractController):
-    def __init__(self, home_view):
+    def __init__(self, home_view, incident):
         self.home_view = home_view
+        self.incident = incident
         self.incident_view = self.load_view("Incident")(self)
+
+    def fill_details(self):
+        timestamp = self.incident.get_incident_timestamp()
+        timestamps = self.incident.get_timestamps()
+        timestamp_data = [timestamp.get_date_time(), timestamp.get_x_acc(),
+                          timestamp.get_y_acc(), timestamp.get_z_acc(),
+                          timestamp.get_svm()]
+        self.incident_view.fill_details_labels(timestamp_data)
+        self.incident_view.fill_details_tree_view(timestamps)
 
     def back(self):
         self.incident_view.close()
--- iotumble/views/incident_view.py
+++ iotumble/views/incident_view.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 
-from PIL import Image, ImageTk
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -14,20 +13,20 @@ class IncidentView(AbstractView, tk.Toplevel):
         self.withdraw()
         self.controller = controller
         self.icon = controller.home_view.icon
-        self.border = tk.Frame()
+        self.header_logo = controller.home_view.header_logo
+        self.details_labels = [ttk.Label()] * 5
         self.details_tree_view = ttk.Treeview()
-        self.header_logo = ImageTk.PhotoImage(Image.open("logo.png"))
         self.columns = ["Timestamp", "X-Acceleration", "Y-Acceleration", "Z-Acceleration", "SVM"]
 
     def load_root(self):
         self.overrideredirect(True)
         self.set_geometry(self, 1200, 800)
         self.configure(background=self.secondary_bg)
-        self.border = tk.Frame(self, background=self.secondary_bg, relief="solid", borderwidth=1)
-        self.border.pack(expand=True, fill="both")
+        self.frame = tk.Frame(self, background=self.secondary_bg, relief="solid", borderwidth=1)
+        self.frame.pack(expand=True, fill="both")
 
     def load_header(self):
-        header_frame = tk.Frame(self.border, background=self.primary_bg)
+        header_frame = tk.Frame(self.frame, background=self.primary_bg)
         header_frame.pack(expand=True, fill="both", side="top")
         header_logo_label = ttk.Label(header_frame, style="primary.TLabel", image=self.header_logo)
         header_logo_label.pack(fill="both", side="left", padx=44)
@@ -43,28 +42,28 @@ class IncidentView(AbstractView, tk.Toplevel):
             widget.bind("<B1-Motion>", lambda e: self.move_window(self, e))
 
     def load_details(self):
-        details_frame = tk.Frame(self.border, background=self.tertiary_bg)
+        details_frame = tk.Frame(self.frame, background=self.tertiary_bg)
         details_frame.pack(expand=True, fill="both", side="left")
-        details_label = ttk.Label(details_frame, style="tertiary.TLabel", text="Incident Details")
-        details_label.pack(fill="both", side="top")
-        details_timestamp_label = ttk.Label(details_frame, style="secondary.TLabel",
-                                            text="Incident Timestamp = ")
-        details_timestamp_label.pack(expand=True, fill="both", padx=(0, 15))
-        details_x_label = ttk.Label(details_frame, style="secondary.TLabel",
-                                    text="Average X-Acceleration = ")
-        details_x_label.pack(expand=True, fill="both", padx=(0, 15))
-        details_y_label = ttk.Label(details_frame, style="secondary.TLabel",
-                                    text="Average Y-Acceleration = ")
-        details_y_label.pack(expand=True, fill="both", padx=(0, 15))
-        details_z_label = ttk.Label(details_frame, style="secondary.TLabel",
-                                    text="Average Z-Acceleration = ")
-        details_z_label.pack(expand=True, fill="both", padx=(0, 15))
-        details_svm_label = ttk.Label(details_frame, style="secondary.TLabel",
-                                      text="Average Signal Vector Magnitude (SVM) = ")
-        details_svm_label.pack(expand=True, fill="both", padx=(0, 15))
-        self.details_tree_view = ttk.Treeview(details_frame, columns=self.columns,
-                                              show="headings", selectmode="none",
-                                              style="details.Treeview")
+        details_title_label = ttk.Label(details_frame, style="tertiary.TLabel",
+                                        text="Incident Details")
+        details_title_label.pack(fill="both", side="top")
+        self.details_labels[0] = ttk.Label(details_frame, style="secondary.TLabel",
+                                           text=self.columns[0])
+        self.details_labels[0].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_labels[1] = ttk.Label(details_frame, style="secondary.TLabel",
+                                           text=self.columns[1])
+        self.details_labels[1].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_labels[2] = ttk.Label(details_frame, style="secondary.TLabel",
+                                           text=self.columns[2])
+        self.details_labels[2].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_labels[3] = ttk.Label(details_frame, style="secondary.TLabel",
+                                           text=self.columns[3])
+        self.details_labels[3].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_labels[4] = ttk.Label(details_frame, style="secondary.TLabel",
+                                           text="Signal Vector Magnitude (SVM)")
+        self.details_labels[4].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_tree_view = ttk.Treeview(details_frame, columns=self.columns, show="headings",
+                                              selectmode="none", style="details.Treeview")
         self.details_tree_view.pack(expand=True, fill="both", side="left")
         details_scrollbar = ttk.Scrollbar(details_frame, orient="vertical",
                                           command=self.details_tree_view.yview)
@@ -76,31 +75,33 @@ class IncidentView(AbstractView, tk.Toplevel):
         for column in self.columns:
             self.details_tree_view.column(column, anchor="center", width=1)
             self.details_tree_view.heading(column, text=column)
+        self.controller.fill_details()
 
     def load_graph(self):
-        graph_label = ttk.Label(self.border, style="tertiary.TLabel", text="Incident Graph")
-        graph_label.pack(fill="both", side="top")
+        graph_title_label = ttk.Label(self.frame, style="tertiary.TLabel", text="Incident Graph")
+        graph_title_label.pack(fill="both", side="top")
         graph = plt.figure(facecolor=self.secondary_bg)
-        graph_canvas = FigureCanvasTkAgg(graph, master=self.border).get_tk_widget()
+        graph_canvas = FigureCanvasTkAgg(graph, master=self.frame).get_tk_widget()
         graph_canvas.pack()
         graph_plot = graph.add_subplot(111)
         self.load_graph_style(graph_plot)
 
     def load_actions(self):
-        actions_frame = tk.Frame(self.border, background=self.secondary_bg)
+        actions_frame = tk.Frame(self.frame, background=self.secondary_bg)
         actions_frame.pack(expand=True, fill="both", side="top", ipadx=60)
-        actions_label = ttk.Label(actions_frame, style="tertiary.TLabel", text="Incident Actions")
-        actions_label.pack(fill="both", side="top")
+        actions_title_label = ttk.Label(actions_frame, style="tertiary.TLabel",
+                                        text="Incident Actions")
+        actions_title_label.pack(fill="both", side="top")
         actions_graph_label = ttk.Label(actions_frame, style="secondary.TLabel",
                                         text="Select Graph")
         actions_graph_label.pack(expand=True, side="left")
         actions_graph_combobox = ttk.Combobox(actions_frame, state="readonly", width=30,
                                               font=(self.font, 12),
-                                              values=["All Acceleration", "X-Acceleration",
-                                                      "Y-Acceleration", "Z-Acceleration",
-                                                      "Signal Vector Magnitude (SVM)"])
+                                              values=["All Acceleration", self.columns[1],
+                                                      self.columns[2], self.columns[3],
+                                                      self.columns[4]])
         actions_graph_combobox.pack(expand=True, side="right")
-        actions_button_frame = tk.Frame(self.border, background=self.secondary_bg)
+        actions_button_frame = tk.Frame(self.frame, background=self.secondary_bg)
         actions_button_frame.pack(expand=True, fill="both", side="bottom")
         actions_graph_button = ttk.Button(actions_button_frame, takefocus=False,
                                           text="Export Graph")
@@ -122,6 +123,18 @@ class IncidentView(AbstractView, tk.Toplevel):
         graph_plot.spines["bottom"].set_color(self.primary_fg)
         graph_plot.spines["left"].set_color(self.primary_fg)
         graph_plot.spines["right"].set_color(self.primary_fg)
+
+    def fill_details_labels(self, timestamp_data):
+        for i, label in enumerate(self.details_labels):
+            label_text = label.cget("text")
+            self.details_labels[i].config(text=f"{label_text} = {str(timestamp_data[i])}")
+
+    def fill_details_tree_view(self, timestamps):
+        for timestamp in timestamps:
+            self.details_tree_view.insert("", "end", tags=str(timestamp.get_timestamp_id() % 2),
+                                          values=(timestamp.get_time(), timestamp.get_x_acc(),
+                                                  timestamp.get_y_acc(), timestamp.get_z_acc(),
+                                                  timestamp.get_svm()))
 
     def stop_tree_view_resize(self, event):
         if self.details_tree_view.identify_region(event.x, event.y) == "separator":
