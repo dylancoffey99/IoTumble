eff58167bd2e8211e9c5e86adf17cb1dad78eaa0
dylancoffey99 <dylancoffey99@gmail.com>
2022-02-16 05:06:41 +0000

    Implemented functionality to Graph section. Created the methods select_graph/plot_graph/set_graph/get_graph_colors in IncidentView. Reduced amount of variables in its constructor, and changed some variable names. Added new details label for Date in details section. Created fill_graph() in IncidentController.

--- iotumble/controllers/incident_controller.py
+++ iotumble/controllers/incident_controller.py
@@ -8,13 +8,30 @@ class IncidentController(AbstractController):
         self.incident_view = self.load_view("Incident")(self)
 
     def fill_details(self):
-        timestamp = self.incident.get_incident_timestamp()
+        timestamp = self.incident.get_max_timestamp()
         timestamps = self.incident.get_timestamps()
-        timestamp_data = [timestamp.get_date_time(), timestamp.get_x_acc(),
-                          timestamp.get_y_acc(), timestamp.get_z_acc(),
-                          timestamp.get_svm()]
-        self.incident_view.fill_details_labels(timestamp_data)
+        self.incident_view.fill_details_labels(timestamp)
         self.incident_view.fill_details_tree_view(timestamps)
+
+    def fill_graph(self, selected_graph):
+        time = self.incident.get_timestamps_time()
+        colors = self.incident_view.get_graph_colors()
+        if selected_graph == "All Acceleration":
+            timestamps_data = [self.incident.get_timestamps_x(), self.incident.get_timestamps_y(),
+                               self.incident.get_timestamps_z()]
+            for i, data in enumerate(timestamps_data):
+                self.incident_view.plot_graph(time, data, colors[i])
+        else:
+            if selected_graph == "X-Acceleration":
+                data = self.incident.get_timestamps_x()
+            elif selected_graph == "Y-Acceleration":
+                data = self.incident.get_timestamps_y()
+            elif selected_graph == "Z-Acceleration":
+                data = self.incident.get_timestamps_z()
+            else:
+                data = self.incident.get_timestamps_svm()
+            self.incident_view.plot_graph(time, data, colors[2])
+        self.incident_view.set_graph(time, selected_graph)
 
     def back(self):
         self.incident_view.close()
--- iotumble/views/incident_view.py
+++ iotumble/views/incident_view.py
@@ -13,9 +13,10 @@ class IncidentView(AbstractView, tk.Toplevel):
         self.withdraw()
         self.controller = controller
         self.icon = controller.home_view.icon
-        self.header_logo = controller.home_view.header_logo
-        self.details_labels = [ttk.Label()] * 5
-        self.details_tree_view = ttk.Treeview()
+        self.details_widgets = [ttk.Label(), ttk.Label(), ttk.Label(),ttk.Label(),
+                                ttk.Label(), ttk.Label(), ttk.Treeview()]
+        self.graph_widgets = [plt.figure(), None]
+        self.texts = ["All Acceleration", "Signal Vector Magnitude"]
         self.columns = ["Timestamp", "X-Acceleration", "Y-Acceleration", "Z-Acceleration", "SVM"]
 
     def load_root(self):
@@ -28,7 +29,8 @@ class IncidentView(AbstractView, tk.Toplevel):
     def load_header(self):
         header_frame = tk.Frame(self.frame, background=self.primary_bg)
         header_frame.pack(expand=True, fill="both", side="top")
-        header_logo_label = ttk.Label(header_frame, style="primary.TLabel", image=self.header_logo)
+        header_logo_label = ttk.Label(header_frame, style="primary.TLabel",
+                                      image=self.controller.home_view.header_logo)
         header_logo_label.pack(fill="both", side="left", padx=44)
         header_options_button = ttk.Button(header_frame, takefocus=False, style="header.TButton",
                                            text="Options")
@@ -47,44 +49,49 @@ class IncidentView(AbstractView, tk.Toplevel):
         details_title_label = ttk.Label(details_frame, style="tertiary.TLabel",
                                         text="Incident Details")
         details_title_label.pack(fill="both", side="top")
-        self.details_labels[0] = ttk.Label(details_frame, style="secondary.TLabel",
-                                           text=self.columns[0])
-        self.details_labels[0].pack(expand=True, fill="both", padx=(0, 15))
-        self.details_labels[1] = ttk.Label(details_frame, style="secondary.TLabel",
-                                           text=self.columns[1])
-        self.details_labels[1].pack(expand=True, fill="both", padx=(0, 15))
-        self.details_labels[2] = ttk.Label(details_frame, style="secondary.TLabel",
-                                           text=self.columns[2])
-        self.details_labels[2].pack(expand=True, fill="both", padx=(0, 15))
-        self.details_labels[3] = ttk.Label(details_frame, style="secondary.TLabel",
-                                           text=self.columns[3])
-        self.details_labels[3].pack(expand=True, fill="both", padx=(0, 15))
-        self.details_labels[4] = ttk.Label(details_frame, style="secondary.TLabel",
-                                           text="Signal Vector Magnitude (SVM)")
-        self.details_labels[4].pack(expand=True, fill="both", padx=(0, 15))
-        self.details_tree_view = ttk.Treeview(details_frame, columns=self.columns, show="headings",
-                                              selectmode="none", style="details.Treeview")
-        self.details_tree_view.pack(expand=True, fill="both", side="left")
+        self.details_widgets[0] = ttk.Label(details_frame, style="secondary.TLabel",
+                                            text="Date")
+        self.details_widgets[0].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_widgets[1] = ttk.Label(details_frame, style="secondary.TLabel",
+                                            text=self.columns[0])
+        self.details_widgets[1].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_widgets[2] = ttk.Label(details_frame, style="secondary.TLabel",
+                                            text=self.columns[1])
+        self.details_widgets[2].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_widgets[3] = ttk.Label(details_frame, style="secondary.TLabel",
+                                            text=self.columns[2])
+        self.details_widgets[3].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_widgets[4] = ttk.Label(details_frame, style="secondary.TLabel",
+                                            text=self.columns[3])
+        self.details_widgets[4].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_widgets[5] = ttk.Label(details_frame, style="secondary.TLabel",
+                                            text=self.texts[1])
+        self.details_widgets[5].pack(expand=True, fill="both", padx=(0, 15))
+        self.details_widgets[6] = ttk.Treeview(details_frame, columns=self.columns,
+                                               show="headings", selectmode="none",
+                                               style="details.Treeview")
+        self.details_widgets[6].pack(expand=True, fill="both", side="left")
         details_scrollbar = ttk.Scrollbar(details_frame, orient="vertical",
-                                          command=self.details_tree_view.yview)
+                                          command=self.details_widgets[6].yview)
         details_scrollbar.pack(fill="both", side="right")
-        self.details_tree_view.configure(yscrollcommand=details_scrollbar.set)
-        self.details_tree_view.tag_configure("0", background=self.secondary_bg)
-        self.details_tree_view.tag_configure("1", background=self.primary_bg)
-        self.details_tree_view.bind("<Button-1>", self.stop_tree_view_resize)
+        self.details_widgets[6].configure(yscrollcommand=details_scrollbar.set)
+        self.details_widgets[6].tag_configure("0", background=self.secondary_bg)
+        self.details_widgets[6].tag_configure("1", background=self.primary_bg)
+        self.details_widgets[6].bind("<Button-1>", self.stop_tree_view_resize)
         for column in self.columns:
-            self.details_tree_view.column(column, anchor="center", width=1)
-            self.details_tree_view.heading(column, text=column)
+            self.details_widgets[6].column(column, anchor="center", width=1)
+            self.details_widgets[6].heading(column, text=column)
         self.controller.fill_details()
 
     def load_graph(self):
         graph_title_label = ttk.Label(self.frame, style="tertiary.TLabel", text="Incident Graph")
         graph_title_label.pack(fill="both", side="top")
-        graph = plt.figure(facecolor=self.secondary_bg)
-        graph_canvas = FigureCanvasTkAgg(graph, master=self.frame).get_tk_widget()
-        graph_canvas.pack()
-        graph_plot = graph.add_subplot(111)
-        self.load_graph_style(graph_plot)
+        graph_figure = plt.figure(facecolor=self.secondary_bg)
+        self.graph_widgets[0] = graph_figure.add_subplot(111)
+        self.graph_widgets[1] = FigureCanvasTkAgg(graph_figure, master=self.frame)
+        graph_plot = self.graph_widgets[1].get_tk_widget()
+        graph_plot.pack()
+        self.load_graph_style()
 
     def load_actions(self):
         actions_frame = tk.Frame(self.frame, background=self.secondary_bg)
@@ -95,12 +102,15 @@ class IncidentView(AbstractView, tk.Toplevel):
         actions_graph_label = ttk.Label(actions_frame, style="secondary.TLabel",
                                         text="Select Graph")
         actions_graph_label.pack(expand=True, side="left")
+        actions_input = tk.StringVar()
         actions_graph_combobox = ttk.Combobox(actions_frame, state="readonly", width=30,
-                                              font=(self.font, 12),
-                                              values=["All Acceleration", self.columns[1],
+                                              font=(self.font, 12), textvariable=actions_input,
+                                              values=[self.texts[0], self.columns[1],
                                                       self.columns[2], self.columns[3],
-                                                      self.columns[4]])
+                                                      self.texts[1]])
         actions_graph_combobox.pack(expand=True, side="right")
+        actions_graph_combobox.bind("<<ComboboxSelected>>",
+                                    lambda e: self.select_graph(actions_input))
         actions_button_frame = tk.Frame(self.frame, background=self.secondary_bg)
         actions_button_frame.pack(expand=True, fill="both", side="bottom")
         actions_graph_button = ttk.Button(actions_button_frame, takefocus=False,
@@ -113,31 +123,57 @@ class IncidentView(AbstractView, tk.Toplevel):
                                            text="Delete Incident")
         actions_delete_button.pack(expand=True, fill="both", side="left", ipadx=2)
 
-    def load_graph_style(self, graph_plot):
-        graph_plot.set(facecolor=self.secondary_bg)
-        graph_plot.xaxis.label.set_color(self.primary_fg)
-        graph_plot.yaxis.label.set_color(self.primary_fg)
-        graph_plot.title.set_color(self.primary_fg)
-        graph_plot.tick_params(color=self.primary_fg, labelcolor=self.primary_fg)
-        graph_plot.spines["top"].set_color(self.primary_fg)
-        graph_plot.spines["bottom"].set_color(self.primary_fg)
-        graph_plot.spines["left"].set_color(self.primary_fg)
-        graph_plot.spines["right"].set_color(self.primary_fg)
+    def load_graph_style(self):
+        self.graph_widgets[0].set(facecolor=self.secondary_bg)
+        self.graph_widgets[0].xaxis.label.set_color(self.primary_fg)
+        self.graph_widgets[0].yaxis.label.set_color(self.primary_fg)
+        self.graph_widgets[0].spines["top"].set_color(self.primary_fg)
+        self.graph_widgets[0].spines["bottom"].set_color(self.primary_fg)
+        self.graph_widgets[0].spines["left"].set_color(self.primary_fg)
+        self.graph_widgets[0].spines["right"].set_color(self.primary_fg)
+        self.graph_widgets[0].tick_params(color=self.primary_fg, labelcolor=self.primary_fg)
 
-    def fill_details_labels(self, timestamp_data):
-        for i, label in enumerate(self.details_labels):
-            label_text = label.cget("text")
-            self.details_labels[i].config(text=f"{label_text} = {str(timestamp_data[i])}")
+    def fill_details_labels(self, timestamp):
+        timestamp_data = [timestamp.get_date(), timestamp.get_time(), timestamp.get_x_acc(),
+                          timestamp.get_y_acc(), timestamp.get_z_acc(), timestamp.get_svm()]
+        for i, label in enumerate(self.details_widgets):
+            if isinstance(label, ttk.Label):
+                label_text = label.cget("text")
+                self.details_widgets[i].config(text=f"[{label_text}]  =  {str(timestamp_data[i])}")
 
     def fill_details_tree_view(self, timestamps):
         for timestamp in timestamps:
-            self.details_tree_view.insert("", "end", tags=str(timestamp.get_timestamp_id() % 2),
-                                          values=(timestamp.get_time(), timestamp.get_x_acc(),
-                                                  timestamp.get_y_acc(), timestamp.get_z_acc(),
-                                                  timestamp.get_svm()))
+            self.details_widgets[6].insert("", "end", tags=str(timestamp.get_timestamp_id() % 2),
+                                           values=(timestamp.get_time(), timestamp.get_x_acc(),
+                                                   timestamp.get_y_acc(), timestamp.get_z_acc(),
+                                                   timestamp.get_svm()))
+
+    def select_graph(self, actions_input):
+        selected_graph = actions_input.get()
+        self.graph_widgets[0].clear()
+        self.graph_widgets[1].draw_idle()
+        self.controller.fill_graph(selected_graph)
+
+    def plot_graph(self, time, data, color):
+        self.graph_widgets[0].plot(time, data, color=color)
+
+    def set_graph(self, time, selected_graph):
+        self.graph_widgets[0].title.set_color(self.primary_fg)
+        self.graph_widgets[0].set(title=selected_graph, xlabel="Time (s)", xlim=(time[0], time[-1]))
+        if selected_graph == self.texts[0]:
+            self.graph_widgets[0].legend(labels=[self.columns[1], self.columns[2], self.columns[3]],
+                                         facecolor=self.secondary_bg, edgecolor=self.secondary_bg,
+                                         labelcolor=self.primary_fg)
+        if selected_graph == self.texts[1]:
+            self.graph_widgets[0].set(ylabel=self.columns[4])
+        else:
+            self.graph_widgets[0].set(ylabel="Acceleration ($m/s^2$)")
+
+    def get_graph_colors(self):
+        return self.primary_fg, self.secondary_fg, self.tertiary_fg
 
     def stop_tree_view_resize(self, event):
-        if self.details_tree_view.identify_region(event.x, event.y) == "separator":
+        if self.details_widgets[6].identify_region(event.x, event.y) == "separator":
             return "break"
         return "continue"
 
@@ -151,6 +187,4 @@ class IncidentView(AbstractView, tk.Toplevel):
         self.mainloop()
 
     def close(self):
-        self.icon.unbind("<Map>")
-        self.icon.unbind("<Unmap>")
         self.destroy()
